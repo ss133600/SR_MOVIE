e6e79f2867fbf5707a29dea414c4fdddc249cf5e
ss133600 <S9878133600@gmail.com>
2023-12-02 23:34:57 +0900

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+  padding: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid white;
+`;
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return <Container>&copy; 2023 SRMOVIE</Container>;
 };
