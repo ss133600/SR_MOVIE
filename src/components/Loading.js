3a3587e9a77069d7dd77ee4cb34e8fe1f3077a12
ss133600 <S9878133600@gmail.com>
2024-01-28 23:05:13 +0900

    loading css 수정2

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  margin: 30 auto;
+  margin: 0 auto;
 
   display: flex;
   justify-content: center;
