72fb0e5c86833adb7b554ee1053e948c40d0744d
ss133600 <S9878133600@gmail.com>
2024-01-31 23:40:14 +0900

    button css수정 2

--- src/components/ButtonUi.js
+++ src/components/ButtonUi.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const Button = styled.button`
   all: unset;
   width: 300px;
-  height: 48px;
+  height: 50px;
   text-align: center;
   background-color: #ff0044;
   border-radius: 10px;
-  margin-top: 13px;
-  font-size: 18px;
+  margin-top: 15px;
+  font-size: 20px;
   font-weight: 600;
   color: white;
   opacity: ${(props) => (props.$isActive ? 1 : 0.5)};
